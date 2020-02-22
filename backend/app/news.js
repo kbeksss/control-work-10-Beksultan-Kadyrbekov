03006df41e3be98f3b132d99e6f5652278d7fab4
kbeksss <kbeksss@outlook.com>
2020-02-22 13:47:49 +0600

    Edited listener for news

---
+++ backend/app/news.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const path = require('path');
+const multer = require('multer');
+const nanoid = require('nanoid');
+
+const mysqlDatabase = require('../mysqlDatabase');
+const config = require('../config');
+const router = express.Router();
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, config.uploadPath);
+    },
+    filename: (req, file, cb) => {
+        cb(null, nanoid() + path.extname(file.originalname))
+    }
+});
+const upload = multer({storage});
+
+router.get('/', async (req, res) => {
+    const news = await mysqlDatabase.getConnection().query('SELECT * FROM `news_posts`');
+    res.send(news);
+});
+router.post('/', async (req, res) => {
+    console.log(req.body);
+    const news_obj = req.body;
+
+    if(req.file){
+        news_obj.image = req.file.filename;
+    }
+
+    if(!news_obj.title || !news_obj.text){
+        return res.send("Missing required keys of title or text")
+    } else {
+        await mysqlDatabase.getConnection().query(
+            'INSERT INTO `news_posts` (`title`, `text`, `image`) VALUES ' +
+            '(?, ?, ?)',
+            [news_obj.title, news_obj.text, news_obj.image]
+        );
+        res.send(news_obj);
+    }
+});
+
+module.exports = router;
--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,26 @@
 const express = require('express');
 
+const app = express();
+
 const cors = require('cors');
-const mysqlDatabase = require('');
+
+const mysqlDatabase = require('./mysqlDatabase');
+const news = require('./app/news');
+const port = 8000;
+
+app.use(express.json());
+app.use(cors());
+app.use(express.static('public'));
+
+app.use('/news', news);
+
+const run = async () => {
+    await mysqlDatabase.connect();
+    app.listen(port, () => {
+        console.log('Server running on', port);
+    });
+    process.on('exit', () => {
+        mysqlDatabase.disconnect();
+    })
+};
+run().catch(e => console.error(e));
